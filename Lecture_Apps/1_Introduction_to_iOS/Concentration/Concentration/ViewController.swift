88f238ef6225c5fdaa316762517f1f55a9b17421
JeffreyJackovich <jeffrey.jackovich@gmail.com>
2017-12-01 23:28:07 -0500

    lecture 1 app commit

---
+++ Lecture_Apps/1_Introduction_to_iOS/Concentration/Concentration/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Concentration
+//
+//  Created by jeffrey jackovich on 12/1/17.
+//  Copyright © 2017 StanfordUniversity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
