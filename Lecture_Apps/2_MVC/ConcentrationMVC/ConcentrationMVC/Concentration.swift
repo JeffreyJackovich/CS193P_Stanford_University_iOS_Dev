8795ea7b5252b1a000af2486830a16ea8163e7a2
JeffreyJackovich <jeffrey.jackovich@gmail.com>
2017-12-02 01:23:55 -0500

    struct and class creation -  min 37:28

---
+++ Lecture_Apps/2_MVC/ConcentrationMVC/ConcentrationMVC/Concentration.swift
@@ -0,0 +1,25 @@
+//
+//  Concentration.swift
+//  ConcentrationMVC
+//
+//  Created by jeffrey jackovich on 12/2/17.
+//  Copyright © 2017 StanfordUniversity. All rights reserved.
+//
+
+import Foundation
+
+class Concentration
+{
+    var cards = [Card]()
+    
+    func chooseCard(at index: Int) {
+        
+    }
+    
+    init(numberOfPairsOfCards: Int) {
+        for identifier in 1...numberOfPairsOfCards {
+            let card = Card()
+            cards += [card, card]
+        }
+    }
+}
